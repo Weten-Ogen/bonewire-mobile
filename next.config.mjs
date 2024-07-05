31e04a8f9a3524e84a666ecf9e6f5e0617fffdfe
Weten-Ogen <alphaoware@gmail.com>
2024-07-05 02:54:53 +0000

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images:{
-       remotePatterns:[
-        {
-            protocol:'https',
-            hostname:'**',
-            pathname: "**",
-            port: ''
-        }
-       ]
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
